d1cdad839d326828feab81a0983007dc37437558
JCSchoeman96 <jcschoeman123@gmail.com>
2026-05-24 11:06:46 +0200

    feat: wire tailwind v4 daisyui esbuild pipeline

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
-// Source Phoenix client entrypoint for Mishka Chelekom generators.
-// EventSales serves the committed counterpart from priv/static/assets/js/app.js.
+import "phoenix_html";
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
 import MishkaComponents from "../vendor/mishka_components.js";
-
-const { Socket } = window.Phoenix;
-const { LiveSocket } = window.LiveView;
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
@@ -11,13 +10,13 @@ let csrfToken = document
 
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
-  params: {
-    _csrf_token: csrfToken,
-  },
-  hooks: {
-    ...MishkaComponents,
-  },
+  params: { _csrf_token: csrfToken },
+  hooks: { ...MishkaComponents },
 });
+
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0,0,0,.3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 liveSocket.connect();
 window.liveSocket = liveSocket;
